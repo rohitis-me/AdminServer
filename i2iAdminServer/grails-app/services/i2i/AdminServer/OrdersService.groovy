9a7bbda6b706bc815f5194639009c5c2c2a60183
Rohit <rohits.iitm@gmail.com>
2015-02-16 13:55:33 +0530

    fixes

--- i2iAdminServer/grails-app/services/i2i/AdminServer/OrdersService.groovy
+++ i2iAdminServer/grails-app/services/i2i/AdminServer/OrdersService.groovy
@@ -49,7 +49,6 @@ class OrdersService {
 		println "ORDERDETAILS: "+order.properties
 		orderDetails.brandId = order.brandId
 		orderDetails.storeId = order.storeId
-		brandDatabaseService.getBrandDataFromBrandId(order.brandId)
 		orderDetails.brandName = brandDatabaseService.getBrandNameFromBrandId(order.brandId)
 		orderDetails.storeName = storeService.getStoreNameFromStoreId(order.storeId)
 		orderDetails.orderId = order.orderId
@@ -160,6 +159,7 @@ class OrdersService {
 	}
 	
 	def getListOfOrdersFromStoreId(String storeId) {
+		println "getListOfOrdersFromStoreId: "+storeId
 		List orderList = Orders.findAllByStoreId(storeId)
 		return orderList
 	}
