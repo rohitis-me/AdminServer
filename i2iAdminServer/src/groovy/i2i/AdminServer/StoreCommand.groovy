3ea8626dd77281ab6355e196dbde3ea83b76b1bb
Rohit <rohits.iitm@gmail.com>
2015-02-18 13:59:42 +0530

    added storecommand

---
+++ i2iAdminServer/src/groovy/i2i/AdminServer/StoreCommand.groovy
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package i2i.AdminServer
+
+import grails.validation.Validateable
+
+/**
+ * @author Rohit2
+ *
+ */
+@Validateable
+class StoreCommand {
+
+	String storeId
+	String storeName
+	String addressLine1
+	String addressLine2
+	String circle
+	String city
+	String state
+	String latitude
+	String longitude
+	
+	
+	static constraints = {
+		storeId nullable:false, blank:false,size:3..100
+		storeName nullable:false, blank:false,size:3..100
+		addressLine1 nullable:false, blank:false,size:3..100
+		addressLine2 nullable:false, blank:false,size:3..100
+		circle nullable:false, blank:false,size:3..100
+		city nullable:false, blank:false,size:1..100
+		state nullable:false, blank:false,size:1..100
+		latitude nullable:true, blank:true,size:1..100
+		longitude nullable:true, blank:true,size:1..100
+	}
+}
